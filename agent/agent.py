dbb69571b48a5adef47ad1c7dd3fb5f01af8c4b7
ganeshmurthy <gmurthy@redhat.com>
2015-06-23 10:38:53 -0400

    Adding tool and utils

--- agent/agent.py
+++ agent/agent.py
@@ -21,69 +21,134 @@
 from proton import Message
 from proton.handlers import MessagingHandler
 from proton.reactor import Container
+from utils import AGENT_BROADCAST_ADDRESS, STATE_FREE, STATE_CLIENT, STATE_SERVER, \
+    STATE_SERVER_CLOSING, DEPLOY_TYPE_CLIENT, DEPLOY_TYPE_SERVER, DEPLOY_TYPE_UNDEPLOY
+
 
 class Agent(MessagingHandler):
+    """
+    Our own custom message handler called Agent that implements reactive message handling semantics.
+    An agent could be a client or a server
+    """
     def __init__(self, url):
         super(Agent, self).__init__()
         self.url = url
         self.senders = {}
-        self.container    = None
-        self.conn         = None
-        self.broadcast_rx = None
-        self.command_rx   = None
+
+        self.container = None
+        self.connection = None
+
+        self.broadcast_receiver = None
+        self.command_receiver = None
+
+        self.receiver = None
+        self.sender = None
+
         self.command_addr = None
-        self.service_rx   = None
-        self.relay        = None
+
+        # This like an abstract sender. You can supply the sender to this relay and have it send messages.
+        self.relay = None
+        self.period = 1.0
+        self.total_requests_received = 0
+        # Default the state of the newly instantiated agent to FREE
+        self.state = STATE_FREE
+        self.address = None
+
+    def get_stats(self):
+        current_state = dict() # Is creating a new dict everytime going to lead a memory leak?
+        current_state['total_requests_received'] = self.total_requests_received
+        current_state['state'] = self.state
+        current_state['outstanding_requests'] = self.receiver.queued
+        # TODO - I am hoping to get the request_rate from ...
+        current_state['request_rate'] = self.total_requests_received / total_uptime
+        return current_state
+
+    def change_state(self, event):
+        deploy_type = event.message.body.get('deploy_type')
+        if self.state == STATE_FREE and deploy_type == DEPLOY_TYPE_SERVER:
+            # Change the state of this agent to SERVER
+            self.state = STATE_SERVER
+            # Open receiver on name
+            self.receiver = event.container.create_receiver(self.connection, self.address)
+        elif self.state == STATE_FREE and deploy_type == DEPLOY_TYPE_CLIENT:
+            # Change the state of this agent to CLIENT
+            self.state = STATE_CLIENT
+            # Open sender to name
+            self.container.create_sender(self.connection, self.address)
+        elif self.state == STATE_SERVER and deploy_type == DEPLOY_TYPE_UNDEPLOY:
+            # Change the state of this agent to FREE
+            self.state = STATE_SERVER_CLOSING
+            # Stop issuing credit for receiver with name
+            #TODO - Check examples to see how you can stop issuing credits
+        elif self.state == STATE_CLIENT and deploy_type == DEPLOY_TYPE_UNDEPLOY:
+            self.state = STATE_FREE
+            # Close sender 'name'
+            self.sender.close()
+        elif self.state == STATE_SERVER_CLOSING and deploy_type == DEPLOY_TYPE_UNDEPLOY:
+            self.state == STATE_FREE
+            self.receiver.close()
+
+    def send_message(self, to, body, correlation_id=None):
+        sender = self.relay or self.senders.get(to)
+
+        if not sender:
+            sender = self.container.create_sender(self.connection, to)
+            self.senders[to] = sender
+
+        sender.send(Message(address=to,
+                            body=body,
+                            correlation_id=correlation_id))
+
+    def process_command(self, event):
+        self.change_state(event)
 
     def on_start(self, event):
         self.container = event.container
-        self.conn = event.container.connect(self.url)
-        self.broadcast_rx = event.container.create_receiver(self.conn, "broadcast/ON_AGENT")
-        self.command_rx   = event.container.create_receiver(self.conn, None, dynamic=True)
+        self.connection = event.container.connect(self.url)
+        #self.broadcast_receiver = event.container.create_receiver(self.conn, AGENT_BROADCAST_ADDRESS)
+        #self.command_receiver = event.container.create_receiver(self.connection, None, dynamic=True)
 
     def on_connection_opened(self, event):
         if event.connection.remote_offered_capabilities and 'ANONYMOUS-RELAY' in event.connection.remote_offered_capabilities:
-            self.relay = self.container.create_sender(self.conn, None)
+            self.relay = self.container.create_sender(self.connection, None)
 
     def on_link_opened(self, event):
-        if event.receiver == self.command_rx:
-            self.command_addr = self.command_rx.remote_source.address
-            print "Agent established on command address: %s" % self.command_addr
+        pass
+        #if event.receiver == self.command_receiver:
+        #    self.command_addr = self.command_receiver.remote_source.address
 
     def on_message(self, event):
-        if event.receiver in (self.command_rx, self.broadcast_rx):
+        """
+        Invoked when messages are received.
+        :param event:
+        :return:
+        """
+        # Increment the total_requests_received by 1, this will include duplicate requests.
+        self.total_requests_received += 1
+
+        # We will only process if the receiver is present in our list of expected receivers.
+        if event.receiver in (self.command_receiver, self.broadcast_receiver):
             self.process_command(event)
-        elif event.receiver == self.service_rx:
+        else:
             pass
 
+    def on_timer_task(self, event):
+        # self.period is defaulted to 1 second. This timer will broadcast a message every one second.
+        self.send_message(AGENT_BROADCAST_ADDRESS, self.get_stats())
+        self.container.schedule(self.period, self)
 
-    def process_command(self, event):
-        body = event.message.body
-        if body.__class__ != dict:
-            return
-        opcode = body['OPCODE']
-        if opcode == 'DISCOVER':
-            self.send_message(event.message.reply_to,
-                              {'OPCODE':'DECLARE', 'ADDR':self.command_addr},
-                              event.message.correlation_id)
-        elif opcode == 'DEPLOY':
-            self.deploy_service(body)
-
-    def send_message(self, to, body, cid=None):
-        sender = self.relay or self.senders.get(to)
-        if not sender:
-            sender = self.container.create_sender(self.conn, to)
-            self.senders[to] = sender
-        sender.send(Message(address=to, body={'OPCODE':'DECLARE', 'ADDR': "%s" % self.command_addr},
-                            correlation_id=cid))
-
-    def deploy_service(self, body):
-        name = body['SVCNAME']
-        self.service_rx = self.container.create_receiver(self.conn, name)
+    #def on_timer(self, event):
+    #    self.period is defaulted to 1 second. This timer will broadcast its stats every one second.
+    #    self.send_message(AGENT_BROADCAST_ADDRESS, self.get_stats())
+    #    self.container.schedule(self.period, self)
 
 try:
-    Container(Agent("0.0.0.0:5672")).run()
-except KeyboardInterrupt: pass
+    agent = Agent("0.0.0.0:5672")
+    container = Container(agent)
+    container.run()
+    #Container(Agent("0.0.0.0:5672")).run()
+except KeyboardInterrupt:
+    container.stop()
 
 
 
---
+++ agent/utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+
+AGENT_BROADCAST_ADDRESS = "broadcast/agent/status"
+TOOL_COMMAND_ADDRESS = "tool/command"
+
+STATE_FREE = "FREE"
+STATE_CLIENT = "CLIENT"
+STATE_SERVER = "SERVER"
+STATE_SERVER_CLOSING = "SERVER_CLOSING"
+
+DEPLOY_TYPE_CLIENT = "DEPLOY_AS_CLIENT"
+DEPLOY_TYPE_SERVER = "DEPLOY_AS_SERVER"
+DEPLOY_TYPE_UNDEPLOY = "UNDEPLOY"
+
+STATES = [STATE_FREE, STATE_CLIENT, STATE_SERVER, STATE_SERVER_CLOSING]
+
+COMMAND_DEPLOY = "DEPLOY"
+COMMAND_UNDEPLOY = "UNDEPLOY"
